32d5bee9f69d6bb37a64c43dc8751500a809561f
Bram De Moor <bram.demoor@gmail.com>
2013-04-19 22:14:42 +0200

    Card toString finish

--- game.js
+++ game.js
@@ -2,31 +2,49 @@ var Bdm;
 (function (Bdm) {
     (function (Estimations) {
         var Suit = (function () {
-            function Suit(name) {
+            function Suit(name, value) {
                 this.name = name;
+                this.value = value;
             }
-            Suit.Hearts = new Suit("Hearts");
-            Suit.Diamonds = new Suit("Diamonds");
-            Suit.Clubs = new Suit("Clubs");
-            Suit.Spades = new Suit("Spades");
+            Suit.Hearts = new Suit("Hearts", 1);
+            Suit.Diamonds = new Suit("Diamonds", 2);
+            Suit.Clubs = new Suit("Clubs", 3);
+            Suit.Spades = new Suit("Spades", 4);
             return Suit;
         })();        
         var Rank = (function () {
-            function Rank() { }
+            function Rank(name, value) {
+                this.name = name;
+                this.value = value;
+            }
+            Rank.Ace = new Rank("Ace", 1);
+            Rank.Two = new Rank("Two", 2);
+            Rank.Three = new Rank("Three", 3);
+            Rank.Four = new Rank("Four", 4);
+            Rank.Five = new Rank("Five", 5);
+            Rank.Six = new Rank("Six", 6);
+            Rank.Seven = new Rank("Seven", 7);
+            Rank.Eight = new Rank("Eight", 8);
+            Rank.Nine = new Rank("Nine", 9);
+            Rank.Ten = new Rank("Ten", 10);
+            Rank.Jack = new Rank("Jack", 11);
+            Rank.Queen = new Rank("Queen", 12);
+            Rank.King = new Rank("King", 13);
             return Rank;
         })();        
         var Card = (function () {
-            function Card(suit) {
+            function Card(suit, rank) {
                 this.suit = suit;
+                this.rank = rank;
             }
             Card.prototype.toString = function () {
-                return this.suit.name;
+                return this.rank.name + ' of ' + this.suit.name;
             };
             return Card;
         })();        
         var Game = (function () {
             function Game() {
-                var myCard = new Card(Suit.Clubs);
+                var myCard = new Card(Suit.Clubs, Rank.King);
                 console.log('Card: ' + myCard.toString());
             }
             return Game;
--- game.ts
+++ game.ts
@@ -1,29 +1,42 @@
 module Bdm.Estimations {
 
     class Suit {
-        static Hearts: Suit = new Suit("Hearts");
-        static Diamonds: Suit = new Suit("Diamonds");
-        static Clubs: Suit = new Suit("Clubs");
-        static Spades: Suit = new Suit("Spades");
+        static Hearts: Suit = new Suit("Hearts", 1);
+        static Diamonds: Suit = new Suit("Diamonds", 2);
+        static Clubs: Suit = new Suit("Clubs", 3);
+        static Spades: Suit = new Suit("Spades", 4);
 
-        constructor(public name: string) {}
+        constructor(public name: string, public value: number) {}
     }
 
     class Rank {
 
+        static Ace: Rank = new Rank("Ace", 1);
+        static Two: Rank = new Rank("Two", 2);
+        static Three: Rank = new Rank("Three", 3);
+        static Four: Rank = new Rank("Four", 4);
+        static Five: Rank = new Rank("Five", 5);
+        static Six: Rank = new Rank("Six", 6);
+        static Seven: Rank = new Rank("Seven", 7);
+        static Eight: Rank = new Rank("Eight", 8);
+        static Nine: Rank = new Rank("Nine", 9);
+        static Ten: Rank = new Rank("Ten", 10);
+        static Jack: Rank = new Rank("Jack", 11);
+        static Queen: Rank = new Rank("Queen", 12);
+        static King: Rank = new Rank("King", 13);
+
+        constructor(public name: string, public value: number) {}
     }
 
-    //enum Ranks { Ace = 1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King  }
-
     class Card {
-        constructor(public suit: Suit) {}
+        constructor(public suit: Suit, public rank: Rank) {}
 
-        toString() { return this.suit.name; }
+        toString() { return this.rank.name + ' of ' + this.suit.name; }
     }
 
     export class Game {
         constructor() {
-            var myCard = new Card(Suit.Clubs);
+            var myCard = new Card(Suit.Clubs, Rank.King);
 
             console.log('Card: ' + myCard.toString());
         }
