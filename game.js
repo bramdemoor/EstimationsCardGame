5569ee28b22f45363c63217cd58b4e8c6a649747
Bram De Moor <bram.demoor@gmail.com>
2013-04-19 22:10:39 +0200

    Card toString

--- game.js
+++ game.js
@@ -1,51 +1,33 @@
 var Bdm;
 (function (Bdm) {
     (function (Estimations) {
-        (function (Suits) {
-            Suits._map = [];
-            Suits._map[0] = "Hearts";
-            Suits.Hearts = 0;
-            Suits._map[1] = "Diamonds";
-            Suits.Diamonds = 1;
-            Suits._map[2] = "Clubs";
-            Suits.Clubs = 2;
-            Suits._map[3] = "Spades";
-            Suits.Spades = 3;
-        })(Estimations.Suits || (Estimations.Suits = {}));
-        var Suits = Estimations.Suits;
-        (function (Ranks) {
-            Ranks._map = [];
-            Ranks._map[0] = "Ace";
-            Ranks.Ace = 0;
-            Ranks._map[1] = "Two";
-            Ranks.Two = 1;
-            Ranks._map[2] = "Three";
-            Ranks.Three = 2;
-            Ranks._map[3] = "Four";
-            Ranks.Four = 3;
-            Ranks._map[4] = "Five";
-            Ranks.Five = 4;
-            Ranks._map[5] = "Six";
-            Ranks.Six = 5;
-            Ranks._map[6] = "Seven";
-            Ranks.Seven = 6;
-            Ranks._map[7] = "Eight";
-            Ranks.Eight = 7;
-            Ranks._map[8] = "Nine";
-            Ranks.Nine = 8;
-            Ranks._map[9] = "Ten";
-            Ranks.Ten = 9;
-            Ranks._map[10] = "Jack";
-            Ranks.Jack = 10;
-            Ranks._map[11] = "Queen";
-            Ranks.Queen = 11;
-            Ranks._map[12] = "King";
-            Ranks.King = 12;
-        })(Estimations.Ranks || (Estimations.Ranks = {}));
-        var Ranks = Estimations.Ranks;
+        var Suit = (function () {
+            function Suit(name) {
+                this.name = name;
+            }
+            Suit.Hearts = new Suit("Hearts");
+            Suit.Diamonds = new Suit("Diamonds");
+            Suit.Clubs = new Suit("Clubs");
+            Suit.Spades = new Suit("Spades");
+            return Suit;
+        })();        
+        var Rank = (function () {
+            function Rank() { }
+            return Rank;
+        })();        
+        var Card = (function () {
+            function Card(suit) {
+                this.suit = suit;
+            }
+            Card.prototype.toString = function () {
+                return this.suit.name;
+            };
+            return Card;
+        })();        
         var Game = (function () {
             function Game() {
-                console.log('hello world!');
+                var myCard = new Card(Suit.Clubs);
+                console.log('Card: ' + myCard.toString());
             }
             return Game;
         })();
--- game.ts
+++ game.ts
@@ -1,10 +1,31 @@
 module Bdm.Estimations {
-    export enum Suits { Hearts, Diamonds, Clubs, Spades }
-    export enum Ranks { Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King  }
+
+    class Suit {
+        static Hearts: Suit = new Suit("Hearts");
+        static Diamonds: Suit = new Suit("Diamonds");
+        static Clubs: Suit = new Suit("Clubs");
+        static Spades: Suit = new Suit("Spades");
+
+        constructor(public name: string) {}
+    }
+
+    class Rank {
+
+    }
+
+    //enum Ranks { Ace = 1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King  }
+
+    class Card {
+        constructor(public suit: Suit) {}
+
+        toString() { return this.suit.name; }
+    }
 
     export class Game {
         constructor() {
-            console.log('hello world!');
+            var myCard = new Card(Suit.Clubs);
+
+            console.log('Card: ' + myCard.toString());
         }
     }
 }
